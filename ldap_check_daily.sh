5025e6abcc94707ab85260de0b8d8bb67fdbc6ba
WannaCryZ <33642971+WannaCryZ@users.noreply.github.com>
2025-06-26 18:33:33 +0800

    Delete ldap_check_daily.sh

--- ldap_check_daily.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-LOG_FILE="/var/www/html/log/ldap_check_$(date +\%Y\%m\%d).log"
-echo "===== 任务触发时间: $(date +'%Y-%m-%d %H:%M:%S') =====" >> $LOG_FILE
-php /var/www/html/check_ldap_users.php >> $LOG_FILE 2>&1
-
